d728296aa887938b273908ef376a93622e7b7168
Ilze <rieba.ilze@gmail.com>
2025-04-06 17:13:26 +0800

    fixed error

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -3,7 +3,6 @@ import "./Portfolio.css";
 import AnimCursor from "./components/AnimCursor";
 import RippleImage from "./RippleImage";
 import Header from "./Header";
-import About from "./About";
 
 export default function Portfolio() {
   return (
