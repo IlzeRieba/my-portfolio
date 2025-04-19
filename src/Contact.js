996941b61feb3e84e67fab237f87f079d94b9466
Ilze <rieba.ilze@gmail.com>
2025-04-19 12:42:09 +0800

    fixing issue

--- src/Contact.js
+++ src/Contact.js
@@ -23,9 +23,7 @@ export default function Contact() {
             {/* LEFT COLUMN with heading */}
             <Col md={6}>
               <div className="leftColumn">
-                <h1 id="contactTitle" className="contactTitle">
-                  CONTACT
-                </h1>
+                <h1 className="contactTitle">CONTACT</h1>
               </div>
             </Col>
 
