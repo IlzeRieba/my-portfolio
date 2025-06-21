6f3d5b5e0ef8931dc13986703bbf83667ce886a7
Ilze <rieba.ilze@gmail.com>
2025-06-21 10:14:37 +0300

    updated contact details

--- src/Contact.js
+++ src/Contact.js
@@ -58,7 +58,7 @@ export default function Contact() {
                     <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />
                     <polyline points="22,6 12,13 2,6" />
                   </svg>
-                  <a href="mailto:rieba.ilze@gmail.com">rieba.ilze@gmail.com</a>
+                  <a href="mailto:rieba.ilze@gmail.com">ilze@oceanwaves-therapy.com  </a>
                 </div>
 
                 {/* WhatsApp Contact */}
